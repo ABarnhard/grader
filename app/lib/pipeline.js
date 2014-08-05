0c95f370e86f101f7b3b5a2e744d3a29ae0db989
Adam Barnhard <admbarn@gmail.com>
2014-08-05 15:46:21 -0500

    enhanced student view directory

--- app/lib/pipeline.js
+++ app/lib/pipeline.js
@@ -3,7 +3,7 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var home = require('../controllers/home');
-//var students = require('../controllers/students');
+var students = require('../controllers/students');
 
 module.exports = function(app, express){
   app.use(morgan('dev'));
@@ -14,6 +14,9 @@ module.exports = function(app, express){
   app.get('/about', home.about);
   app.get('/faq', home.faq);
   app.get('/contact', home.contact);
+
+  app.get('/students/new', students.init);
+  app.post('/students/new', students.create);
 
   console.log('Pipeline Configured');
 };
